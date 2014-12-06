cd4c16f28b286507a58b3fdf204cfe5f68ea091f
martin <martin.rixham@gmail.com>
2014-12-06 08:35:44 +0000

    Bindings outside of the mutation are not reapplied.

--- src/BindingRoot.js
+++ src/BindingRoot.js
@@ -2,6 +2,54 @@ function BindingRoot(model) {
 
 	this.assertUniqueness();
 
+	var injectProperty = function(key, property) {
+
+		var datum = new Datum(property);
+
+		Object.defineProperty(model, key, {
+
+			get: function() { return datum(); },
+			set: function(value) { datum(value); }
+		});
+	};
+
+	// This loop is responsible for rebinding the data structure
+	// after dom mutations.
+	// It reapplies every binding though need not be applied to the 
+	// root object.
+	var rebind = function(model) {
+
+		for(var key in model) {
+
+			var property = model[key];
+
+			if (property && property.isBinding) {
+	
+				property.rebind(model._scope, key);
+			}
+			else if (property && typeof(property) == "object") {
+
+				var element =
+					model._scope.querySelector("[data-bind=" + key + "]");
+
+				if (element) {
+
+					property._scope = element;
+
+					rebind(property);
+				}
+			}
+		}
+	};
+
+	var attachRebinder = function(element, model) {
+
+		element._rebind = function() {
+	
+			rebind(model);
+		};
+	};
+
 	// This loop is responsible for binding the data structure
 	// both initially when the binding root is created and
 	// after a new binding is added.
@@ -13,18 +61,9 @@ function BindingRoot(model) {
 		if (newBinding) {
 
 			model._scope = scope;
+
+			attachRebinder(scope, model);
 		}
-
-		var injectProperty = function(key, property) {
-
-			var datum = new Datum(property);
-
-			Object.defineProperty(model, key, {
-
-				get: function() { return datum(); },
-				set: function(value) { datum(value); }
-			});
-		};
 
 		for(var key in model)
 		{
@@ -60,13 +99,10 @@ function BindingRoot(model) {
 		bindObject(scope, model);
 	});
 
-	// This loop is responsible for rebinding the data structure
-	// after dom mutations.
-	// It reapplies every binding.
 	var forceRebind = function(model) {
 
-		for(var key in model)
-		{
+		for(var key in model) {
+
 			var property = model[key];
 
 			if (property && property.isBinding) {
@@ -75,7 +111,8 @@ function BindingRoot(model) {
 			}
 			else if (property && typeof(property) == "object") {
 
-				var element = model._scope.querySelector("[data-bind=" + key + "]");
+				var element =
+					model._scope.querySelector("[data-bind=" + key + "]");
 
 				if (element) {
 
@@ -89,19 +126,35 @@ function BindingRoot(model) {
 
 	var observer = new MutationObserver(function(mutations) {
 
-		var notTextMutation = 
-			mutations.some(function(mutation) { 
+		var mutation = mutations[0];
 
-				return mutation.target.children.length; 
-			});
+		var notTextMutation = mutation.target.children.length; 
 
 		if (notTextMutation) {
 
-			forceRebind(model);
+			var element = mutation.target;
+
+			while (element) {
+
+				if (element._rebind) {
+
+					element._rebind();
+					break;
+				}
+				else {
+
+					element = element.parentElement;
+				}
+			}
 		}
 	});
 
 	observer.observe(scope, { childList: true, subtree: true });
+
+	this.disconnect = function() {
+
+		observer.disconnect();
+	};
 }
 
 BindingRoot.prototype = new UniqueRoot();
