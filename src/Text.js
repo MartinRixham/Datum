cfd96c174b88f708218bdf22eec9aa3ee22d1294
Martin <martin.rixham@gmail.com>
2015-01-04 11:06:51 +0000

    Text binding only accepts functions.

--- src/Text.js
+++ src/Text.js
@@ -4,35 +4,25 @@ function Text(text) {
 
 		var elements = scope.querySelectorAll("[data-bind=" + name + "]");
 
-		if (typeof(text) == "function") {
-
-			this.requestRegistrations();
+		this.requestRegistrations();
 		
+		var evaluated = text.call(model);
+
+		this.assignUpdater(function() {
+		
+			var elements = scope.querySelectorAll("[data-bind=" + name + "]");
+
 			var evaluated = text.call(model);
-
-			this.assignUpdater(function() {
-		
-				var elements = scope.querySelectorAll("[data-bind=" + name + "]");
-
-				var evaluated = text.call(model);
-
-				for (var i = 0; i < elements.length; i++) {
-
-					elements[i].textContent = evaluated;
-				}
-			});
 
 			for (var i = 0; i < elements.length; i++) {
 
 				elements[i].textContent = evaluated;
 			}
-		}
-		else {
+		});
 
-			for (var j = 0; j < elements.length; j++) {
+		for (var i = 0; i < elements.length; i++) {
 
-				elements[j].textContent = text;
-			}
+			elements[i].textContent = evaluated;
 		}
 	};
 }
