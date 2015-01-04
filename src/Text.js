a981fdd1aaaeaa447a92949cee695caeb4986829
Martin <martin.rixham@gmail.com>
2015-01-04 11:20:09 +0000

    Access element from text binding.

--- src/Text.js
+++ src/Text.js
@@ -1,28 +1,24 @@
 function Text(text) {
 
+	this.createCallback = function(model, element) {
+
+		this.assignUpdater(function() {
+
+			element.textContent = text.call(model, element);
+		});
+	};
+
 	this.bind = function(scope, name, model) {
 
 		var elements = scope.querySelectorAll("[data-bind=" + name + "]");
-
-		this.requestRegistrations();
 		
-		var evaluated = text.call(model);
-
-		this.assignUpdater(function() {
-		
-			var elements = scope.querySelectorAll("[data-bind=" + name + "]");
-
-			var evaluated = text.call(model);
-
-			for (var i = 0; i < elements.length; i++) {
-
-				elements[i].textContent = evaluated;
-			}
-		});
-
 		for (var i = 0; i < elements.length; i++) {
 
-			elements[i].textContent = evaluated;
+			this.requestRegistrations();
+
+			elements[i].textContent = text.call(model, elements[i]);
+
+			this.createCallback(model, elements[i]);
 		}
 	};
 }
