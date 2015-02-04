77f7781338bda836eca0609d92bc83ea3db85dab
MartinRixham <MartinRixham@users.noreply.github.com>
2015-02-04 16:06:39 +0000

    Binding interface commment.

--- src/Binding.js
+++ src/Binding.js
@@ -3,7 +3,18 @@ function Binding(bindings) {
 	this.requestRebind();
 
 	setTimeout(this.rebindDataStructure);
-
+	
+	// The applyBinding method is the sole member of the binding interface,
+	// the most important internal interface in Datum.
+	// Is is implemented by many different objects and is the library's
+	// main point of extensibility.
+	// The key principle of the interface is that responsibility is left to
+	// the implementation to determine whether it is appropriate for
+	// the binding to be applied when the method is called.
+	// The binding itself makes sure that it is applied correctly,
+	// for example that it is not inappropriately applied multiple times.
+	// The client code is responsible only for telling the binding where to bind
+	// and signalling that the binding may need to be applied.
 	this.applyBinding = function(scope, name, model) {
 
 		if (bindings.text) {
