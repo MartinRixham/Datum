c8749e81de0e3a678250b99138d07254e46a0f8a
MartinRixham <martin.rixham@gmail.com>
2016-08-20 12:04:41 +0100

    Remove children when object null.

---
+++ src/ObjectBinding.js
@@ -0,0 +1,46 @@
+define(["Subscriber"], function(Subscriber) {
+
+	function ObjectBinding() {
+
+		var elementChildren = [];
+
+		this.setUpElement = function(parentModel, element) {
+
+			var children = [].slice.call(element.childNodes);
+
+			elementChildren.push({ "element": element, "children": children });
+		};
+
+		this.updateElement = function(parentModel, element, key) {
+
+			var model = parentModel[key];
+
+			if (!model) {
+
+				var children = element.children;
+
+				for (var i = children.length - 1; i >= 0; i--) {
+
+					element.removeChild(children[i]);
+				}
+			}
+		};
+
+		this.resetElement = function(element) {
+
+			for (var i = 0; i < elementChildren.length; i++) {
+
+				var children = elementChildren[i];
+
+				if (element == children.element) {
+
+					elementChildren.splice(i, 1);
+				}
+			}
+		};
+	}
+
+	ObjectBinding.prototype = new Subscriber();
+
+	return ObjectBinding;
+});
