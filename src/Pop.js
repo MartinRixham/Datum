27616fc755a8b5780f6317bf27568b7bc19177d3
MartinRixham <martin.rixham@gmail.com>
2016-10-09 21:23:00 +0100

    Remove property when popping from bound array.

--- src/Pop.js
+++ src/Pop.js
@@ -1,16 +1,23 @@
 define([], function() {
 
-	function Pop(model, elementChildren) {
+	function Pop(model, elementChildren, properties) {
 
 		var originalPop = model.pop;
 
 		model.pop = function() {
 
-			var element = elementChildren[0].element;
+			var property = properties.pop();
 
-			element.removeChild(element.firstElementChild);
+			property.removeBinding();
 
-			originalPop.apply(this, arguments);
+			for (var i = 0; i < elementChildren.length; i++) {
+
+				var element = elementChildren[i].element;
+
+				element.removeChild(element.firstElementChild);
+
+				originalPop.apply(this, arguments);
+			}
 		};
 	}
 
