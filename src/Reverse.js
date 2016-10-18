5a64c3f764111142710e1257664efadfa27cd5b4
MartinRixham <martin.rixham@gmail.com>
2016-10-18 22:00:07 +0100

    Reverse array length two.

--- src/Reverse.js
+++ src/Reverse.js
@@ -2,6 +2,22 @@ define([], function() {
 
 	function Reverse(model, elementChildren, properties) {
 
+		var originalReverse = model.reverse;
+
+		model.reverse = function() {
+
+			for (var i = 0; i < elementChildren.length; i++) {
+
+				var element = elementChildren[i].element;
+				var firstChild = element.firstElementChild;
+
+				element.remove(firstChild);
+				element.appendChild(firstChild);
+			}
+
+			properties.reverse();
+			originalReverse.apply(this, arguments);
+		};
 	}
 
 	return Reverse;
