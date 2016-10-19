a56c068a117cb8a4175d14e18def35e122a75814
MartinRixham <martin.rixham@gmail.com>
2016-10-19 22:16:44 +0100

    Sort elements.

--- src/Sort.js
+++ src/Sort.js
@@ -4,24 +4,90 @@ define([], function() {
 
 		model.sort = function(comparison) {
 
-			var modelProperties = [];
+			var modelChildrenProperties = getModelChildrenProperties();
 
-			for (var i = 0; i < model.length; i++) {
-
-				modelProperties.push({ model: model[i], property: properties[i] });
-			}
-
-			modelProperties.sort(function(a, b) {
+			modelChildrenProperties.sort(function(a, b) {
 
 				return comparison(a.model, b.model);
 			});
 
-			for (i = 0; i < model.length; i++) {
-
-				model[i] = modelProperties[i].model;
-				properties[i] = modelProperties[i].property;
-			}
+			replaceSortedObjects(modelChildrenProperties);
 		};
+
+		function getModelChildrenProperties() {
+
+			var allChildren = getAllChildren();
+			var modelChildrenProperties = [];
+
+			for (var i = 0; i < model.length; i++) {
+
+				modelChildrenProperties.push({
+
+					model: model[i],
+					property: properties[i],
+					children: allChildren[i]
+				});
+			}
+
+			return modelChildrenProperties;
+		}
+
+		function getAllChildren() {
+
+			var allChildren = [];
+
+			for (var i = 0; i < elementChildren.length; i++) {
+
+				var element = elementChildren[i].element;
+				var children = [].slice.call(element.children);
+
+				removeChildren(element);
+
+				for (var j = 0; j < children.length; j++) {
+
+					if (!allChildren[j]) {
+
+						allChildren[j] = [];
+					}
+
+					allChildren[j][i] = children[j];
+				}
+			}
+
+			return allChildren;
+		}
+
+		function removeChildren(element) {
+
+			while (element.lastChild) {
+
+				element.removeChild(element.lastChild);
+			}
+		}
+
+		function replaceSortedObjects(modelChildrenProperties) {
+
+			for (var i = 0; i < model.length; i++) {
+
+				model[i] = modelChildrenProperties[i].model;
+				properties[i] = modelChildrenProperties[i].property;
+
+				for (var j = 0; j < elementChildren.length; j++) {
+
+					var element = elementChildren[j].element;
+
+					replaceChildren(element, modelChildrenProperties[i].children);
+				}
+			}
+		}
+
+		function replaceChildren(element, children) {
+
+			for (var i = 0; i < children.length; i++) {
+
+				element.appendChild(children[i]);
+			}
+		}
 	}
 
 	return Sort;
