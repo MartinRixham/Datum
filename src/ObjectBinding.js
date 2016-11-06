4346468a04d0e6e39a731ca5abd3da29c3c5f4f9
MartinRixham <martin.rixham@gmail.com>
2016-11-06 17:21:03 +0000

    Refactor object binding.

--- src/ObjectBinding.js
+++ src/ObjectBinding.js
@@ -13,37 +13,49 @@ define([], function() {
 
 		this.updateElement = function(parentModel, element, model) {
 
-			var children;
-
 			if (model) {
 
-				for (var i = 0; i < elementChildren.length; i++) {
-
-					if (elementChildren[i].element == element) {
-
-						children = elementChildren[i].children;
-						break;
-					}
-				}
-
-				if (!element.childNodes.length) {
-
-					for (var j = 0; j < children.length; j++) {
-
-						element.appendChild(children[j]);
-					}
-				}
+				replaceChildren(element);
 			}
 			else {
 
-				children = element.childNodes;
-
-				for (var k = children.length - 1; k >= 0; k--) {
-
-					element.removeChild(children[k]);
-				}
+				removeChildren(element);
 			}
 		};
+
+		function replaceChildren(element) {
+
+			var children = getChildren(element);
+
+			if (!element.childNodes.length) {
+
+				for (var i = 0; i < children.length; i++) {
+
+					element.appendChild(children[i]);
+				}
+			}
+		}
+
+		function getChildren(element) {
+
+			for (var i = 0; i < elementChildren.length; i++) {
+
+				if (elementChildren[i].element == element) {
+
+					return elementChildren[i].children;
+				}
+			}
+		}
+
+		function removeChildren(element) {
+
+			var children = element.childNodes;
+
+			for (var i = children.length - 1; i >= 0; i--) {
+
+				element.removeChild(children[i]);
+			}
+		}
 
 		this.resetElement = function(element) {
 
